dd196bda031ed250c49158694eed8f18e38f7d3a
Julian Rojas <jfrojas251@gmail.com>
2022-01-27 13:32:32 -0500

    Change import momentTz

--- src/modules/order/order.service.ts
+++ src/modules/order/order.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
-import * as momentTz from 'moment-timezone';
+import momentTz from 'moment-timezone';
 import { Order } from '../../entities';
 import { OrderDetailService } from '../order-detail/orderDetail.service';
 import { AllieDetailService } from '../allie-detail/allieDetail.service';
