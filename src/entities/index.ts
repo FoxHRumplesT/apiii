6031db00b2d56a4bfb630878bba0dfbcf076a27a
Julian Rojas <jfrojas251@gmail.com>
2022-01-27 13:12:06 -0500

    Change routes 5

--- src/entities/index.ts
+++ src/entities/index.ts
@@ -1,17 +1,37 @@
-export * from './allie-detail-category.entity';
-export * from './allie-detail-type.entity';
-export * from './allie-detail.entity';
-export * from './allie-menu-allie-detail.entity';
-export * from './allie-menu.entity';
-export * from './allie-schedule.entity';
-export * from './allie-step.entity';
-export * from './allie-type.entity';
-export * from './allie.entity';
-export * from './city.entity';
-export * from './country.entity';
-export * from './order-detail.entity';
-export * from './order.entity';
-export * from './rol.entity';
-export * from './state.entity';
-export * from './suscription.entity';
-export * from './user.entity';
+import { AllieDetailCategory } from './allie-detail-category.entity';
+import { AllieDetailType } from './allie-detail-type.entity';
+import { AllieDetail } from './allie-detail.entity';
+import { AllieMenuAllieDetail } from './allie-menu-allie-detail.entity';
+import { AllieMenu } from './allie-menu.entity';
+import { AllieSchedule } from './allie-schedule.entity';
+import { AllieStep } from './allie-step.entity';
+import { AllieType } from './allie-type.entity';
+import { Allie } from './allie.entity';
+import { City } from './city.entity';
+import { Country } from './country.entity';
+import { OrderDetail } from './order-detail.entity';
+import { Order } from './order.entity';
+import { Rol } from './rol.entity';
+import { State } from './state.entity';
+import { Suscription } from './suscription.entity';
+import { User } from './user.entity';
+
+export {
+  AllieDetailCategory,
+  AllieDetailType,
+  AllieDetail,
+  AllieMenuAllieDetail,
+  AllieMenu,
+  AllieSchedule,
+  AllieStep,
+  AllieType,
+  Allie,
+  City,
+  Country,
+  OrderDetail,
+  Order,
+  Rol,
+  State,
+  Suscription,
+  User
+}
